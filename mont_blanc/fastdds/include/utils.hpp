5e4535eeeb08f38f919341fde8e33433b86561ec
methylDragon <brandon@openrobotics.org>
2022-03-01 17:21:31 -0800

    Add more utils for random msg generation

--- mont_blanc/fastdds/include/utils.hpp
+++ mont_blanc/fastdds/include/utils.hpp
@@ -1,11 +1,13 @@
 #ifndef _UTILS_H_
 #define _UTILS_H_
 
-#include "types/datatypes.h"
-
 #include <random>
+#include <vector>
 #include <algorithm>
 #include <limits>
+
+#include "types/datatypes.h"
+
 
 namespace montblanc
 {
@@ -24,7 +26,7 @@ std::string random_string( size_t length )
         const size_t max_index = (sizeof(charset) - 1);
         return charset[ rand() % max_index ];
     };
-    std::string str(length,0);
+    std::string str(length, 0);
     std::generate_n( str.begin(), length, randchar );
     return str;
 }
@@ -40,7 +42,26 @@ T random_number(T min = std::numeric_limits<T>::min(), T max = std::numeric_limi
   return dist(gen);
 }
 
-Header random_header(int len = 16)
+template <typename T,
+          typename = typename std::enable_if<std::is_arithmetic<T>::value, T>::type >
+std::vector<T> random_number_vector(
+  size_t length,
+  T min = std::numeric_limits<T>::min(),
+  T max = std::numeric_limits<T>::max())
+{
+  auto randnum = [&]() -> T
+  {
+    return random_number<T>(min, max);
+  };
+
+  std::vector<T> out(length);
+  std::generate(out.begin(), out.end(), randnum);
+
+  return out;
+}
+
+// MSG GEN ==============================================================================================================
+Header random_header(size_t len = 16)
 {
     Header header_msg;
     header_msg.sec(random_number<long>());
@@ -49,6 +70,25 @@ Header random_header(int len = 16)
 
     return header_msg;
 }
+
+Image random_image(size_t len = 0) // 1920 * 1080 * 3
+{
+    Image image_msg;
+
+    image_msg.header(montblanc::random_header());
+
+    image_msg.height(montblanc::random_number<unsigned long>());
+    image_msg.width(montblanc::random_number<unsigned long>());
+
+    image_msg.encoding(montblanc::random_string(32));
+    image_msg.is_bigendian(montblanc::random_number<int>(0, 1));
+    image_msg.step(montblanc::random_number<unsigned long>());
+
+    image_msg.data(montblanc::random_number_vector<uint8_t>(len));
+
+    return image_msg;
+}
+
 
 } // namespace montblanc
 
